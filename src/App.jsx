feceb2cb233a848c83847759c36108f151ec0805
Anuj Kumar <anujy5706@gmail.com>
2024-09-02 11:52:07 +0530

    Added login page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Auth/Signup";
+import Login from "./Pages/Auth/Login";
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
      <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/auth/login" element={<Login />} />
+
      </Routes>
 
 
--- src/Pages/Auth/Signup.jsx
+++ src/Pages/Auth/Signup.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 function Signup() {
         return (
             <>
@@ -232,8 +234,9 @@ function Signup() {
 
              <p className="mt-3 text-xs text-gray-500 " >
              Already have an account? 
-             <a href="/login" className="text-yellow-500">Login</a>
-             
+            
+             <Link to="/auth/login" className="text-yellow-500">Login</Link>
+
              </p>
 
              </form>
