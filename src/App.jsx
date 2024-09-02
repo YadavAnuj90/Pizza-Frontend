881800839acde9f435d03503602fee3f913dd24e
Anuj Kumar <anujy5706@gmail.com>
2024-09-02 12:04:11 +0530

    Added NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./Pages/Home";
 import Signup from "./Pages/Auth/Signup";
 import Login from "./Pages/Auth/Login";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,10 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/auth/signup" element={<Signup />} />
       <Route path="/auth/login" element={<Login />} />
+
+
+
+      <Route path="*" element={<NotFound />} />
 
      </Routes>
 
