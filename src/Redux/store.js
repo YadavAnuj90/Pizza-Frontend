6db8f4b0066bfe89d897518c6152dd87aca8289c
Anuj Kumar <anujy5706@gmail.com>
2024-09-08 23:04:37 +0530

    resolve

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -3,17 +3,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import AuthSliceReducer from "./Slices/AuthSlice";
 import ProductSliceReducer from "./Slices/ProductSlice";
 import CartSliceReducer from "./Slices/CartSlice";
+
 import OrderSliceReducer from "./Slices/OrderSlice";
+
 export const store = configureStore({
-    reducer: {
+     reducer: {
         auth: AuthSliceReducer,
-        product: ProductSliceReducer,
+        product:ProductSliceReducer,
         cart: CartSliceReducer,
-        order: OrderSliceReducer
-    },
-    devTools: true,
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware({
-          serializableCheck: false,
-        }),
+        order:OrderSliceReducer
+
+     },
+     devTools:true,
+     middleware: getDefaultMiddleware =>
+      getDefaultMiddleware({
+         serializableCheck: false,
+      }),
+
 });
